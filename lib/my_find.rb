443dfc1cf5720589953ee9ff77369148cf1505ee
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 20:52:18 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i += 1
+  end
 end
+
+my_find(collection) { |i| 1 % 3 == 0 and 1 % 5 == 0 }
